baf12c0fd67e972a031566aadf065b08daa7d1f3
Matthew Knight <mattknight.sa@gmail.com>
2020-01-31 13:14:31 -0600

    updated model

--- models/user.js
+++ models/user.js
@@ -14,6 +14,13 @@ module.exports = function(sequelize, DataTypes) {
              len: [1]
          }
       },
+      github: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
+     }
       email: {
         type: DataTypes.STRING,
         allowNull: false,
