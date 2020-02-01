d5e7ab496a54ea9d11f98505da44dd737a93822c
Matthew Knight <mattknight.sa@gmail.com>
2020-02-01 15:34:09 -0600

    update

--- models/post.js
+++ models/post.js
@@ -1,43 +1,26 @@
 module.exports = function(sequelize, DataTypes) {
-    var Post = sequelize.define("Post", {
-      link: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      blurb: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-        validate: {
-          len: [1]
-        }
+  var Post = sequelize.define("Post", {
+    link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
       }
-    });
-    Post.associate = function(models) {
-      Post.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-    Post.associate = function(models) {
-      Post.belongsTo(models.Tag, {
-        as: 'child_tag',
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-    
-    Post.associate = function(models) {
-      Post.belongsTo(models.Tag, {
-        as: 'parent_tag',
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-   return Post;
-  };
+    },
+    blurb: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        len: [1]
+      }
+    },
+    tagId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+ 
+  
+  
+ return Post;
+};
--- models/tags.js
+++ models/tags.js
@@ -1,19 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
-    var Tag = sequelize.define("Tags", {
+var Tag = sequelize.define("Tag", {
+
+  title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+          len: [1,20]
+      }
+   },
+   
   
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1,20]
-            }
-         },
-         parent: {
-           type: DataTypes.INTEGER,
-           allowNull: true
-         },
-         
-  
-    });
-    return Tag;
-  };
+});
+return Tag;
+};
