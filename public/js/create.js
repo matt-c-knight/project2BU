3cb6f1ebdffebe225dd4ec3ac1a5c4c31a4dbbbc
Matthew Knight <mattknight.sa@gmail.com>
2020-02-05 12:18:09 -0600

    update

--- public/js/create.js
+++ public/js/create.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var selected;
-  $('#my-select').change(function(){
-     selected = $(this).find(":selected").data("tag");
-   console.log(selected)
- 
+  $('#my-select').change(function () {
+    selected = $(this).find(":selected").data("tag");
+    console.log(selected)
+
   });
   $("#submit-post").click(function (event) {
     event.preventDefault();
@@ -11,22 +11,22 @@ $(document).ready(function() {
     var nameInput = $(".name").val();
     var linkInput = $(".link").val().trim();
     var textInput = $(".text").val().trim();
-    
-        var newPost = {
-          username: nameInput,
-          link: linkInput,
-          blurb: textInput,
-          tagId: selected
-      }
-      console.log(newPost)
-        function createPosts(data) {
-          $.post("/api/posts", data)
-              .then(
-                  console.log(data)
-              );
-      }
-      createPosts(newPost)
-   
-});
+
+    var newPost = {
+      username: nameInput,
+      link: linkInput,
+      blurb: textInput,
+      tagId: selected
+    }
+    console.log(newPost)
+    function createPosts(data) {
+      $.post("/api/posts", data)
+        .then(
+          console.log(data)
+        );
+    }
+    createPosts(newPost)
+
+  });
 
 });
