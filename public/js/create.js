49f03a5227e45bb5af173f0ae2eb99be2f986f07
Matthew Knight <mattknight.sa@gmail.com>
2020-02-06 11:07:34 -0600

    update

--- public/js/create.js
+++ public/js/create.js
@@ -1,32 +1,34 @@
-$(document).ready(function () {
-  var selected;
-  $('#my-select').change(function () {
-    selected = $(this).find(":selected").data("tag");
-    console.log(selected)
 
+
+$(document).ready(function() {
+    var selected;
+    $('#my-select').change(function(){
+       selected = $(this).find(":selected").data("tag");
+     console.log(selected)
+   
+    });
+    $("#submit-post").click(function (event) {
+      event.preventDefault();
+      console.log(selected)
+      // var nameInput = $(".name").val();
+      var linkInput = $(".link").val().trim();
+      var textInput = $(".text").val().trim();
+      
+          var newPost = {
+            // username: nameInput,
+            link: linkInput,
+            blurb: textInput,
+            tagId: selected
+        }
+        console.log(newPost)
+          function createPosts(data) {
+            $.post("/api/posts", data)
+                .then(
+                    console.log(data)
+                );
+        }
+        createPosts(newPost)
+     
   });
-  $("#submit-post").click(function (event) {
-    event.preventDefault();
-    console.log(selected)
-    // var nameInput = $(".name").val();
-    var linkInput = $(".link").val().trim();
-    var textInput = $(".text").val().trim();
-
-    var newPost = {
-      // username: nameInput,
-      link: linkInput,
-      blurb: textInput,
-      tagId: selected
-    }
-    console.log(newPost)
-    function createPosts(data) {
-      $.post("/api/posts", data)
-        .then(
-          console.log(data)
-        );
-    }
-    createPosts(newPost)
-
+  
   });
-
-});
